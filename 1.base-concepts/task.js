c4e1ff90532f4ef8a99f55799e0313cd76c334bb
Romashko555 <alkom55@mail.ru>
2022-01-28 00:31:30 +0600

    testCommit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,29 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  "use strict";
+  let D = b**2-4*a*c;
+  if (D < 0) {
+    arr = [];
+  }
+  else if (D === 0) {
+    arr = [-b/(2*a)];
+  }
+  else if (D > 0) {   
+    arr = [(-b + Math.sqrt(D)) /(2*a), (-b - Math.sqrt(D))/(2*a)];
+  }
+  else {
+    console.log("неизвестная ошибка");
+  }
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+
+//function calculateTotalMortgage(percent, contribution, amount, date) {
+//  let totalAmount;
+
+//   код для задачи №2 писать здесь
+
+//  return totalAmount;
+//}
