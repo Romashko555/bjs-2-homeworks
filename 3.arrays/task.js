7c8183a0df55ab9d0eed8d322d267a77df31577f
Romashko555 <alkom55@mail.ru>
2022-02-07 21:23:58 +0600

    test commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+let result;
+result = (arr1.length == arr2.length && arr1.every((a, i) => arr2[i] == a));
+console.log(result);
+return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+let resultArr;
+resultArr = arr.filter((arr) => arr % 3 === 0 && arr > 0).map((arr) => arr * 10);
+console.log(resultArr);
+return resultArr;
 }
