58da40c88b87d75aeb7ccccbfb4471709603f3f1
Romashko555 <alkom55@mail.ru>
2022-02-13 20:54:28 +0600

    test commit4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,50 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  }
+  else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (mark1, ...marks) {
+  if (this.marks === undefined) {
+    this.marks = [mark1, ...marks];
+  }
+  else {
+    this.marks.push(mark1, ...marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+//проверки
+// let Student1 = new Student ("Вася", "Петров", "22");
+// Student1.setSubject("algebra");
+// Student1.addMarks(4, 0);
+// Student1.addMark(4);
+// Student1.addMark(6);
+// Student1.addMarks(1, 2);
+// Student1.getAverage();
+// console.log(Student1);
+// console.log(Student1.getAverage());
+// Student1.exclude(1);
+// console.log(Student1);
